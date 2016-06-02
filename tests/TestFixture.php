772fd49a1fe3b1d04aee94572a766f87d67fd522
Andrey Artyshenko <work.a.andrey@gmail.com>
2016-06-02 17:03:28 +0500

    Unit tests wrapper added

---
+++ tests/TestFixture.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class TestFixture
+ */
+
+class TestFixture extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        
+    }
+
+    protected function tearDown()
+    {
+
+    }
+
+}
