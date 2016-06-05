f3e83365bb5abeaf85111c7a38e5f800e7df6e8d
Andrey Artyshenko <work.a.andrey@gmail.com>
2016-06-05 15:10:58 +0500

    Add Exceptions

---
+++ src/Exceptions/ApiCall/WrongContent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 05.06.16
+ * Time: 14:40
+ */
+
+namespace Language\Exceptions\ApiCall;
+
+
+class WrongContent extends \Exception
+{
+
+}
